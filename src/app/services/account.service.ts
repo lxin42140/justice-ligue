31df4e35227eb2e32ebed47866b4960cce012c89
Li Xin <50946087+lxin42140@users.noreply.github.com>
2022-07-02 10:40:10 +0800

    added fake data

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,3 +1,4 @@
+import { DummyLawyer, DummyClient } from './../data/data';
 import { Injectable } from '@angular/core';
 import { Client } from '../models/Client';
 import { LawAreaEnum } from '../models/enums/law-area.enum';
@@ -17,45 +18,8 @@ export class AccountService {
 
   constructor() { 
     this.isClient = true;
-    this.lawyer = {
-      firstName: 'John',
-      lastName: 'Smith',
-      email: 'johnsmith@gmail.com',
-      yearsOfExperience: 10,
-      practiceAreas: [{
-        areaName: LawAreaEnum.CRIMINAL,
-        areaDescription: '',
-        associatedLegalIssues: [{
-          issueName: LegalIssueEnum.DRUG_OFFENCES,
-          issueDescription: '',
-          associatedLegalArea: [{
-            areaName: LawAreaEnum.CRIMINAL,
-            areaDescription: '',
-            associatedLegalIssues: []
-          }]
-        }]
-      }],
-      experiencedIssues: [{
-        issueName: LegalIssueEnum.DRUG_OFFENCES,
-        issueDescription: '',
-        associatedLegalArea: [{
-          areaName: LawAreaEnum.CRIMINAL,
-          areaDescription: '',
-          associatedLegalIssues: []
-        }]
-      }],
-      caseTemplates: [],
-      budgets: [],
-      cases: [],
-      scheduledMeetings: []
-    }
-    this.client = {
-      firstName: 'Adam',
-      lastName: 'Pie',
-      email: 'johnsmith@gmail.com',
-      cases: [],
-      scheduledMeetings: []
-    }
+    this.lawyer = DummyLawyer;
+    this.client = DummyClient;
   }
 
   public switchUser(): void {
--- src/app/services/lawyers.service.ts
+++ src/app/services/lawyers.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LawAreaEnum } from '../models/enums/law-area.enum';
-import { LegalIssueEnum } from '../models/enums/legal-issue.enum';
+import { DummyLawyer } from '../data/data';
 import { Lawyer } from '../models/Lawyer';
 
 @Injectable({
@@ -11,37 +10,8 @@ export class LawyersService {
   allLawyers: Lawyer[] = [];
 
   constructor() {
-    this.allLawyers = [{
-      firstName: 'John',
-      lastName: 'Smith',
-      email: 'johnsmith@gmail.com',
-      yearsOfExperience: 10,
-      practiceAreas: [{
-        areaName: LawAreaEnum.CRIMINAL,
-        areaDescription: '',
-        associatedLegalIssues: [{
-          issueName: LegalIssueEnum.DRUG_OFFENCES,
-          issueDescription: '',
-          associatedLegalArea: [{
-            areaName: LawAreaEnum.CRIMINAL,
-            areaDescription: '',
-            associatedLegalIssues: []
-          }]
-        }]
-      }],
-      experiencedIssues: [{
-        issueName: LegalIssueEnum.DRUG_OFFENCES,
-        issueDescription: '',
-        associatedLegalArea: [{
-          areaName: LawAreaEnum.CRIMINAL,
-          areaDescription: '',
-          associatedLegalIssues: []
-        }]
-      }],
-      caseTemplates: [],
-      budgets: [],
-      cases: [],
-      scheduledMeetings: []
-    }]
+    this.allLawyers = [
+      DummyLawyer
+    ]
   }
 }
